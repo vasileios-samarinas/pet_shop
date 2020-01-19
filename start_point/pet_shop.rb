35684e231432ed4c9df1fa22ebe772672a579366
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-01-19 15:21:55 +0000

    Test increase_pets_sold passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -13,12 +13,19 @@ end
 #to add or remove cash from total cash
 #of the pet shop
 def add_or_remove_cash(shop,money)
-return shop[:admin][:total_cash]+= money
-return shop[:admin][:total_cash]-= money
+  return shop[:admin][:total_cash]+= money
+  return shop[:admin][:total_cash]-= money
 end
 
 #The below is a function
 #that shows how many pets sold
 def pets_sold(shop)
   return shop[:admin][:pets_sold]
+end
+
+#The below is a function
+#that shows how to increase the
+#number of pets sold
+def increase_pets_sold(shop,number_of_pets)
+  shop[:admin][:pets_sold]+=(number_of_pets)
 end
--- start_point/specs/pet_shop_spec.rb
+++ start_point/specs/pet_shop_spec.rb
@@ -101,17 +101,17 @@ class TestPetShop < Minitest::Test
     cash = total_cash(@pet_shop)
     assert_equal(990, cash)
   end
-  
+
   def test_pets_sold
     sold = pets_sold(@pet_shop)
     assert_equal(0, sold)
   end
 
-  # def test_increase_pets_sold
-  #   increase_pets_sold(@pet_shop,2)
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(2, sold)
-  # end
+  def test_increase_pets_sold
+    increase_pets_sold(@pet_shop,2)
+    sold = pets_sold(@pet_shop)
+    assert_equal(2, sold)
+  end
   #
   # def test_stock_count
   #   count = stock_count(@pet_shop)
