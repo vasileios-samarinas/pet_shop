a81d9021544367e820702de3f99c3a45c7bec154
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-01-19 14:43:41 +0000

    Test_pet_shop_name passed

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,4 @@
+#The below is a function to get the name of the pet shop
+def pet_shop_name(name)
+  return name[:name]
+end
--- start_point/specs/pet_shop_spec.rb
+++ start_point/specs/pet_shop_spec.rb
@@ -89,91 +89,91 @@ class TestPetShop < Minitest::Test
   #   sum = total_cash(@pet_shop)
   #   assert_equal(1000, sum)
   # end
-
+  #
   # def test_add_or_remove_cash__add
   #   add_or_remove_cash(@pet_shop,10)
   #   cash = total_cash(@pet_shop)
   #   assert_equal(1010, cash)
   # end
-
+  #
   # def test_add_or_remove_cash__remove
   #   add_or_remove_cash(@pet_shop,-10)
   #   cash = total_cash(@pet_shop)
   #   assert_equal(990, cash)
   # end
-
+  #
   # def test_pets_sold
   #   sold = pets_sold(@pet_shop)
   #   assert_equal(0, sold)
   # end
-
+  #
   # def test_increase_pets_sold
   #   increase_pets_sold(@pet_shop,2)
   #   sold = pets_sold(@pet_shop)
   #   assert_equal(2, sold)
   # end
-
+  #
   # def test_stock_count
   #   count = stock_count(@pet_shop)
   #   assert_equal(6, count)
   # end
-
+  #
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
   #   assert_equal(2, pets.count)
   # end
-
+  #
   # def test_all_pets_by_breed__not_found
   #   pets = pets_by_breed(@pet_shop, "Dalmation")
   #   assert_equal(0, pets.count)
   # end
-
+  #
   # def test_find_pet_by_name__returns_pet
   #   pet = find_pet_by_name(@pet_shop, "Arthur")
   #   assert_equal("Arthur", pet[:name])
   # end
-
+  #
   # def test_find_pet_by_name__returns_nil
   #   pet = find_pet_by_name(@pet_shop, "Fred")
   #   assert_nil(pet)
   # end
-
+  #
   # def test_remove_pet_by_name
   #   remove_pet_by_name(@pet_shop, "Arthur")
   #   pet = find_pet_by_name(@pet_shop,"Arthur")
   #   assert_nil(pet)
   # end
-
+  #
   # def test_add_pet_to_stock
   #   add_pet_to_stock(@pet_shop, @new_pet)
   #   count = stock_count(@pet_shop)
   #   assert_equal(7, count)
   # end
-
+  #
   # def test_customer_cash
   #   cash = customer_cash(@customers[0])
   #   assert_equal(1000, cash)
   # end
-
+  #
   # def test_remove_customer_cash
   #   customer = @customers[0]
   #   remove_customer_cash(customer, 100)
   #   assert_equal(900, customer[:cash])
   # end
-
+  #
   # def test_customer_pet_count
   #   count = customer_pet_count(@customers[0])
   #   assert_equal(0, count)
   # end
-
+  #
   # def test_add_pet_to_customer
   #   customer = @customers[0]
   #   add_pet_to_customer(customer, @new_pet)
   #   assert_equal(1, customer_pet_count(customer))
   # end
-
-  # --- OPTIONAL ---
-
+  #
+  # # --- OPTIONAL ---
+  #
   # def test_customer_can_afford_pet__sufficient_funds
   #   customer = @customers[0]
   #   can_buy_pet = customer_can_afford_pet(customer, @new_pet)
