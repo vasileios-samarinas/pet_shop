b5d8db94395cdb016b73db60791cfa2f77c273ce
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-01-19 15:05:44 +0000

    Add_or_remove_cash_test passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -8,3 +8,11 @@ end
 def total_cash(shop)
   return shop[:admin][:total_cash]
 end
+
+#The below is a function
+#to add or remove cash from total cash
+#of the pet shop
+def add_or_remove_cash(shop,money)
+return shop[:admin][:total_cash]+= money
+return shop[:admin][:total_cash]-= money
+end
